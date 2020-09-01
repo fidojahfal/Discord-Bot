fe77143463d19fddbf54fc1c1f214956fa889b05
fidojahfal <42795148+fidojahfal@users.noreply.github.com>
2020-09-01 10:43:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,8 @@ client.on('ready', ()=>{
 });
 
 client.on('message', async (message)=>{
-    const gif = new MessageAttachment('https://media.giphy.com/media/H99r2HtnYs492/giphy.gif')
+    const gif = new MessageAttachment('https://media.giphy.com/media/H99r2HtnYs492/giphy.gif');
+    const gif2 = new MessageAttachment('https://media.giphy.com/media/yJFeycRK2DB4c/giphy.gif')
     if(message.author.bot) return;
     console.log(`[${message.author.tag}]: ${message.content}`);
     if(message.content === 'Halo'){
@@ -50,6 +51,19 @@ client.on('message', async (message)=>{
             console.log(err)
             message.channel.send('I dont have permission or the user was not found');
         }
+    }else if(CMD_NAME == 'unban'){
+        if(!message.member.hasPermission('BAN_MEMBERS'))
+            return message.reply('You dont have permissions to use that command');
+        
+        if(args.length === 0) 
+            return message.reply('Please provide an ID');
+            try{
+                const user = await message.guild.members.unban(args[0]);
+                message.reply('That user was unbanned', gif2);
+            }catch(err){
+                console.log(err)
+                message.channel.send('I dont have permission or the user was not found');
+            }
     }
   }
 })
